ecb35a284523a436510b1bcf6d2150a5b1bd588e
sgjung <playmymind@daum.net>
2021-01-01 15:24:37 +0900

    문자열 압축 문제 : 문자열 단위별 순환 구문 작성 : 문자열 단위별 갯수 카운팅 구문 추가

--- src/main/java/programmers/stringcompression/iterationBased/IterStringCompression.java
+++ src/main/java/programmers/stringcompression/iterationBased/IterStringCompression.java
@@ -1,17 +1,21 @@
 package programmers.stringcompression.iterationBased;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IterStringCompression {
 
 	public static int answer(String question){
 		int result = 0;
+		Map<String, Integer> countMap = new HashMap<String, Integer>();
 
 		for (int step = 1; step<question.length(); step++){
-			String outer_current = question.substring(0, step);
+			int count = 0;
+			String prev = question.substring(0, step);
 			int subLimit = (int) Math.floor(question.length() / step);
+			System.out.println("outer :: " + prev);
 
-			System.out.println("outer_current :: " + outer_current);
-
-			for (int i = 1; i<=subLimit; i++){
+			for (int i = 1; i<=subLimit && subLimit!=1; i++){
 				// beginIndex) substring 의 beginIndex
 				// substring 을 하는 단위는 step 의 배수로 늘려나간다.
 				int beginIndex = (i-1)*step;
@@ -24,16 +28,29 @@ public class IterStringCompression {
 				String inner_current = "";
 
 				// endIndex가 문자열의 범위를 넘어서게 되는 경우
-				if (endIndex >= question.length()){
-					inner_current = question.substring(beginIndex);
-				}
+//				if (endIndex >= question.length()){
+//					inner_current = question.substring(beginIndex);
+//				}
 				// endIndex가 문자열의 범위를 넘어서지 않는 경우
-				else {
+//				else {
 					inner_current = question.substring(beginIndex, endIndex);
-				}
-				System.out.println("inner_current ("+ i +") :: " + inner_current);
+					if(prev.equals(inner_current)){
+//						if("de".equals(prev)){
+//							System.out.println(">>> prev :: " + prev + ", inner_current :: " + inner_current);
+//						}
+						count++;
+						countMap.put(prev, count);
+					}
+					else {
+						// 나머지 문자열에 대해서도 동일하게 찾는다. (step = i 단위로 해서 찾는다.)
+						prev = inner_current;
+					}
+//				}
+				System.out.println("inner :: " + prev);
+
 			}
 		}
+		System.out.println("answer :: " + countMap.toString());
 
 		return result;
 	}
@@ -46,8 +63,17 @@ public class IterStringCompression {
 		String question4 = "abcabcabcabcdededededede";
 		String question5 = "xababcdcdababcdcd";
 
-		System.out.println("============== question1");
-		System.out.println(answer(question1));
+//		System.out.println("============== question1");
+//		System.out.println(answer(question1));
+
+//		System.out.println("============== question2");
+//		System.out.println(answer(question2));
+
+//		String test = "abcde";
+//		System.out.println(test.substring(2,test.length()));
+
+		System.out.println("============== question3 :: " + question3);
+		System.out.println(answer(question3));
 	}
 
 }
