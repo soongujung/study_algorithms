6b55e3776ac2c4c68c597b00e962f58b4cc8a283
sgjung <playmymind@daum.net>
2021-01-01 13:40:45 +0900

    문자열 압축 문제 : 문자열 단위별 순환 구문 작성 : 주석 추가

--- src/main/java/programmers/stringcompression/iterationBased/IterStringCompression.java
+++ src/main/java/programmers/stringcompression/iterationBased/IterStringCompression.java
@@ -6,27 +6,32 @@ public class IterStringCompression {
 		int result = 0;
 
 		for (int step = 1; step<question.length(); step++){
-			String first = question.substring(0, step);
+			String outer_current = question.substring(0, step);
 			int subLimit = (int) Math.floor(question.length() / step);
-			System.out.println("first :: " + first);
+
+			System.out.println("outer_current :: " + outer_current);
 
 			for (int i = 1; i<=subLimit; i++){
+				// beginIndex) substring 의 beginIndex
+				// substring 을 하는 단위는 step 의 배수로 늘려나간다.
 				int beginIndex = (i-1)*step;
+
+				// endIndex) substring 의 endIndex
+				// i*step 이 지정된 배열의 단위를 넘어서는 경우가 있기에 이에 대한 처리구문 추가
 				int endIndex = (i*step) > question.length() ? question.length() : i*step;
 
-				String next = "";
+				// 내부 for 루프 안에서 돌아가는 current
+				String inner_current = "";
 
-				if (endIndex == question.length()){
-					next = question.substring(beginIndex);
+				// endIndex가 문자열의 범위를 넘어서게 되는 경우
+				if (endIndex >= question.length()){
+					inner_current = question.substring(beginIndex);
 				}
-				else if (endIndex > question.length()){
-					next = question.substring(beginIndex);
-				}
+				// endIndex가 문자열의 범위를 넘어서지 않는 경우
 				else {
-					next = question.substring(beginIndex, endIndex);
+					inner_current = question.substring(beginIndex, endIndex);
 				}
-
-				System.out.println("next ("+ i +") :: " + next);
+				System.out.println("inner_current ("+ i +") :: " + inner_current);
 			}
 		}
 
