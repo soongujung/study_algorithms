3c2c16cdc963a173c4cf9a1bea6b63cd2178363e
sgjung <playmymind@daum.net>
2021-01-15 09:07:09 +0900

    그래프 (dfs)

---
+++ src/main/java/basic/adjacent_list_based/sample.kt
@@ -0,0 +1,15 @@
+package basic.adjacent_list_based
+
+
+data class Node (
+    val position: Int,
+    val weight: Int
+)
+
+
+fun main(args: Array<String>): Unit{
+    val v0_1 = Node(position = 1, weight = 7)
+    val v0_2 = Node(position = 2, weight = 5)
+
+
+}
